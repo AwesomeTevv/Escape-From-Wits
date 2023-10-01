dabec8f20b5b4d671ab63ccf0082bdcd0b407b87
Akshar Nana <chapboomclash@gmail.com>
2023-10-01 22:31:07 +0200

    Added main menu and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { LevelOneComponent } from './level-one/level-one.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "mainMenu",component: MainMenuComponent},
+  {path: "levelOne", component: LevelOneComponent},
+  {path: "**", redirectTo:"mainMenu",pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
