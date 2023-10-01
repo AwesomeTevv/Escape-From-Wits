c90f6971b144850f01891ce3e09ce4e3ed285163
AwesomeTevv <tevlen.naidoo@gmail.com>
2023-10-01 21:22:52 +0200

    Added beta components

---
+++ src/app/level-one/level-one.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-level-one',
+  templateUrl: './level-one.component.html',
+  styleUrls: ['./level-one.component.css']
+})
+export class LevelOneComponent {
+
+}
