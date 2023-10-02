d6100f1cdfc5b6cc365b386d10fbda840be50f51
AwesomeTevv <tevlen.naidoo@gmail.com>
2023-10-02 15:52:46 +0200

    Something rather than nothing

--- src/app/level-one/level-one.component.ts
+++ src/app/level-one/level-one.component.ts
@@ -1,9 +1,19 @@
 import { Component, HostListener } from '@angular/core';
-import RAPIER, { ColliderDesc, RigidBody, RigidBodyDesc, World } from '@dimforge/rapier3d';
-import { CONTROLLER_BODY_RADIUS, CharacterControls } from 'src/utilities/characterControls';
+import RAPIER, {
+  ColliderDesc,
+  RigidBody,
+  RigidBodyDesc,
+  World,
+} from '@dimforge/rapier3d';
+import {
+  CONTROLLER_BODY_RADIUS,
+  CharacterControls,
+} from 'src/utilities/characterControls';
 import { body } from 'src/utilities/utils';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { FirstPersonControls } from 'three/examples/jsm/controls/FirstPersonControls';
+import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 @Component({
@@ -11,14 +21,13 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
   templateUrl: './level-one.component.html',
   styleUrls: ['./level-one.component.css'],
 })
-
 export class LevelOneComponent {
-  togglePause = false;  
+  togglePause = false;
   @HostListener('window:keyup', ['$event'])
   keyEvent(event: KeyboardEvent) {
-    if(event.key === "Escape"){
-      console.log("Toggled Pause!\n");
-      this.togglePause = !this.togglePause;  
+    if (event.key === 'Escape') {
+      console.log('Toggled Pause!\n');
+      this.togglePause = !this.togglePause;
     }
   }
 }
@@ -39,20 +48,25 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.set(10, 10, 20);
-camera.lookAt(0, 0, 0);
+camera.position.set(10, 3, 10);
+// camera.position.set(10, 10, 20);
+// camera.lookAt(0, 0, 0);
+
+// const fpsCamera = new FirstPersonCamera(camera, )
 
 /**
  * Controls
  */
-const orbitcontrols = new OrbitControls(camera, renderer.domElement);
+const controls = new PointerLockControls(camera, renderer.domElement);
+// controls.lookSpeed = 2;
+// controls.movementSpeed = 5;
 // orbitcontrols.enableDamping = true;
 // orbitcontrols.minDistance = 2;
 // orbitcontrols.maxDistance = 3;
 // orbitcontrols.enablePan = false;
 // orbitcontrols.maxPolarAngle = Math.PI / 2 - 0.05;
 // orbitcontrols.minPolarAngle = Math.PI / 4;
-orbitcontrols.update();
+// controls.update();
 
 const loader = new GLTFLoader();
 
@@ -67,10 +81,10 @@ dLight.castShadow = true;
 dLight.shadow.mapSize.width = 100;
 dLight.shadow.mapSize.height = 100;
 const d = 35;
-dLight.shadow.camera.left = - d;
+dLight.shadow.camera.left = -d;
 dLight.shadow.camera.right = d;
 dLight.shadow.camera.top = d;
-dLight.shadow.camera.bottom = - d;
+dLight.shadow.camera.bottom = -d;
 scene.add(dLight);
 
 const aLight = new THREE.AmbientLight('white', 0.4);
@@ -95,11 +109,11 @@ let gravity = { x: 0.0, y: -9.81, z: 0.0 };
 let world = new RAPIER.World(gravity);
 
 // Bodys
-const bodys: { rigid: RigidBody, mesh: THREE.Mesh }[] = [];
+const bodys: { rigid: RigidBody; mesh: THREE.Mesh }[] = [];
 
 // Creating Ground Plane
 let scale = new RAPIER.Vector3(100.0, 3.0, 100.0);
-const planeGeometry = new THREE.PlaneGeometry(100,100);
+const planeGeometry = new THREE.PlaneGeometry(100, 100);
 const planeMaterial = new THREE.MeshPhongMaterial({
   color: 0xffff00,
   side: THREE.DoubleSide,
@@ -112,121 +126,169 @@ scene.add(plane);
 
 let groundBodyDesc = RAPIER.RigidBodyDesc.fixed();
 let groundBody = world.createRigidBody(groundBodyDesc);
-let groundCollider = RAPIER.ColliderDesc.cuboid(
-  50,0,50
-);
-world.createCollider(groundCollider,groundBody);
+let groundCollider = RAPIER.ColliderDesc.cuboid(50, 0, 50);
+world.createCollider(groundCollider, groundBody);
 
 // Creating Some Shape
-const staticB = body(scene, world, 'kinematicPositionBased', 'cube',
-{ hx: 10, hy: 0.8, hz: 10 }, { x: 0, y: 2.5, z: 0 },
-{ x: 0, y: 0, z: Math.PI/2 }, 'pink');
+const staticB = body(
+  scene,
+  world,
+  'kinematicPositionBased',
+  'cube',
+  { hx: 10, hy: 0.8, hz: 10 },
+  { x: 0, y: 2.5, z: 0 },
+  { x: 0, y: 0, z: Math.PI / 2 },
+  'pink'
+);
 bodys.push(staticB);
 
-const cubeBody = body(scene, world, 'dynamic', 'cube',
-{ hx: 0.5, hy: 0.5, hz: 0.5 }, { x: 0, y: 15, z: 0 },
-{ x: 0, y: 0.4, z: 0.7 }, 'orange');
+const cubeBody = body(
+  scene,
+  world,
+  'dynamic',
+  'cube',
+  { hx: 0.5, hy: 0.5, hz: 0.5 },
+  { x: 0, y: 15, z: 0 },
+  { x: 0, y: 0.4, z: 0.7 },
+  'orange'
+);
 bodys.push(cubeBody);
 
-const sphereBody = body(scene, world, 'dynamic', 'sphere',
-{ radius: 0.7 }, { x: 4, y: 15, z: 2 },
-{ x: 0, y: 1, z: 0 }, 'blue');
+const sphereBody = body(
+  scene,
+  world,
+  'dynamic',
+  'sphere',
+  { radius: 0.7 },
+  { x: 4, y: 15, z: 2 },
+  { x: 0, y: 1, z: 0 },
+  'blue'
+);
 bodys.push(sphereBody);
 
-const sphereBody2 = body(scene, world, 'dynamic', 'sphere',
-{ radius: 0.7 }, { x: 0, y: 15, z: 0 },
-{ x: 0, y: 1, z: 0 }, 'red');
+const sphereBody2 = body(
+  scene,
+  world,
+  'dynamic',
+  'sphere',
+  { radius: 0.7 },
+  { x: 0, y: 15, z: 0 },
+  { x: 0, y: 1, z: 0 },
+  'red'
+);
 bodys.push(sphereBody2);
 
-const cylinderBody = body(scene, world, 'dynamic', 'cylinder',
-{ hh: 1.0, radius: 0.7 }, { x: -7, y: 15, z: 8 },
-{ x: 0, y: 1, z: 0 }, 'green');
+const cylinderBody = body(
+  scene,
+  world,
+  'dynamic',
+  'cylinder',
+  { hh: 1.0, radius: 0.7 },
+  { x: -7, y: 15, z: 8 },
+  { x: 0, y: 1, z: 0 },
+  'green'
+);
 bodys.push(cylinderBody);
 
-const coneBody = body(scene, world, 'dynamic', 'cone',
-{ hh: 1.0, radius: 1 }, { x: 7, y: 15, z: -8 },
-{ x: 0, y: 1, z: 0 }, 'purple');
+const coneBody = body(
+  scene,
+  world,
+  'dynamic',
+  'cone',
+  { hh: 1.0, radius: 1 },
+  { x: 7, y: 15, z: -8 },
+  { x: 0, y: 1, z: 0 },
+  'purple'
+);
 bodys.push(coneBody);
 
 // Character
-var characterControls: CharacterControls;
-loader.load('../../assets/Soldier.glb', function(gltf){
-  const model = gltf.scene;
-  model.traverse(function (object: any){
-    if(object.isMesh){
-      object.castShadow = true;
-    }
-  });
-  model.position.set(10,0,10);
-  scene.add(model);
-  const gltfAnimations: THREE.AnimationClip[] = gltf.animations;
-  const mixer = new THREE.AnimationMixer(model);
-  const animationsMap: Map<string, THREE.AnimationAction> = new Map()
-  gltfAnimations.filter(a => a.name != 'TPose').forEach((a: THREE.AnimationClip) => {
-      animationsMap.set(a.name, mixer.clipAction(a))
-  })
+// var characterControls: CharacterControls;
+// loader.load('../../assets/Soldier.glb', function (gltf) {
+//   const model = gltf.scene;
+//   model.traverse(function (object: any) {
+//     if (object.isMesh) {
+//       object.castShadow = true;
+//     }
+//   });
+//   model.position.set(10, 0, 10);
+//   scene.add(model);
+//   const gltfAnimations: THREE.AnimationClip[] = gltf.animations;
+//   const mixer = new THREE.AnimationMixer(model);
+//   const animationsMap: Map<string, THREE.AnimationAction> = new Map();
+//   gltfAnimations
+//     .filter((a) => a.name != 'TPose')
+//     .forEach((a: THREE.AnimationClip) => {
+//       animationsMap.set(a.name, mixer.clipAction(a));
+//     });
 
+//   // RIGID BODY
+//   let bodyDesc = RAPIER.RigidBodyDesc.kinematicPositionBased().setTranslation(
+//     10,
+//     0,
+//     10
+//   );
+//   let rigidBody = world.createRigidBody(bodyDesc);
+//   let dynamicCollider = RAPIER.ColliderDesc.ball(CONTROLLER_BODY_RADIUS);
+//   world.createCollider(dynamicCollider, rigidBody);
 
-  // RIGID BODY
-  let bodyDesc = RAPIER.RigidBodyDesc.kinematicPositionBased().setTranslation(10, 0, 10)
-  let rigidBody = world.createRigidBody(bodyDesc);
-  let dynamicCollider = RAPIER.ColliderDesc.ball(CONTROLLER_BODY_RADIUS);
-  world.createCollider(dynamicCollider, rigidBody);
-
-  characterControls = new CharacterControls(model, mixer, 
-      animationsMap, orbitcontrols, 
-      camera,  'Idle',
-      new RAPIER.Ray( 
-          { x: 0, y: 0, z: 0 },
-          { x: 0, y: -1, z: 0} 
-      ),
-      new RAPIER.Ray(
-        {x: 0, y: 0, z: 0},
-        {x: 0, y: 0, z: 1}
-        ),
-      new RAPIER.Ray(
-        {x: 0, y: 0, z: 0},
-        {x: 1, y: 0, z: 0}
-      ),rigidBody)
-});
+//   characterControls = new CharacterControls(
+//     model,
+//     mixer,
+//     animationsMap,
+//     controls,
+//     camera,
+//     'Idle',
+//     new RAPIER.Ray({ x: 0, y: 0, z: 0 }, { x: 0, y: -1, z: 0 }),
+//     new RAPIER.Ray({ x: 0, y: 0, z: 0 }, { x: 0, y: 0, z: 1 }),
+//     new RAPIER.Ray({ x: 0, y: 0, z: 0 }, { x: 1, y: 0, z: 0 }),
+//     rigidBody
+//   );
+// });
 
 const clock = new THREE.Clock();
 function animate() {
   let deltaT = clock.getDelta();
 
-  if (characterControls) {
-    characterControls.update(world, deltaT, keysPressed);
-}
+  // if (characterControls) {
+  //   characterControls.update(world, deltaT, keysPressed);
+  // }
 
   world.step();
-  bodys.forEach(body => {
+  bodys.forEach((body) => {
     let position = body.rigid.translation();
     let rotation = body.rigid.rotation();
 
-    body.mesh.position.x = position.x
-    body.mesh.position.y = position.y
-    body.mesh.position.z = position.z
+    body.mesh.position.x = position.x;
+    body.mesh.position.y = position.y;
+    body.mesh.position.z = position.z;
 
     body.mesh.setRotationFromQuaternion(
-        new THREE.Quaternion(rotation.x,
-            rotation.y,
-            rotation.z,
-            rotation.w));
+      new THREE.Quaternion(rotation.x, rotation.y, rotation.z, rotation.w)
+    );
   });
- 
+
   requestAnimationFrame(animate);
-  orbitcontrols.update();
+  // controls.update(clock.getDelta());
   renderer.render(scene, camera);
 }
 animate();
 
-const keysPressed: any = {}
-document.addEventListener('keydown', (event) => {
-    if (event.shiftKey && characterControls) {
-        characterControls.switchRunToggle()
-    }
-    keysPressed[event.key.toLowerCase()] = true
-}, false);
-document.addEventListener('keyup', (event) => {
-    keysPressed[event.key.toLowerCase()] = false
-}, false);
+// const keysPressed: any = {};
+// document.addEventListener(
+//   'keydown',
+//   (event) => {
+//     if (event.shiftKey && characterControls) {
+//       characterControls.switchRunToggle();
+//     }
+//     keysPressed[event.key.toLowerCase()] = true;
+//   },
+//   false
+// );
+// document.addEventListener(
+//   'keyup',
+//   (event) => {
+//     keysPressed[event.key.toLowerCase()] = false;
+//   },
+//   false
+// );
