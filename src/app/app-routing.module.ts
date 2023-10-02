697cbd3ff6580d942c764b391f52f3e2ff361a23
AwesomeTevv <tevlen.naidoo@gmail.com>
2023-10-02 08:21:29 +0200

    Made it so level one is shown first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { MainMenuComponent } from './main-menu/main-menu.component';
 import { LevelOneComponent } from './level-one/level-one.component';
 
 const routes: Routes = [
-  { path: 'mainMenu', component: MainMenuComponent },
   { path: 'levelOne', component: LevelOneComponent },
+  { path: 'mainMenu', component: MainMenuComponent },
   // {path: "**", redirectTo:"mainMenu",pathMatch:'full'}
   { path: '**', redirectTo: 'levelOne', pathMatch: 'full' }, // Temporary change so that Level One is shown by default
 ];
