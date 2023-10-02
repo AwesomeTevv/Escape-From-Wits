005e97a182cd6774f18a0e5872c1e0f7965e7f8c
AwesomeTevv <tevlen.naidoo@gmail.com>
2023-10-02 08:33:16 +0200

    Added basic components

--- src/app/level-one/level-one.component.ts
+++ src/app/level-one/level-one.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import * as THREE from 'three';
 
 @Component({
   selector: 'app-level-one',
   templateUrl: './level-one.component.html',
-  styleUrls: ['./level-one.component.css']
+  styleUrls: ['./level-one.component.css'],
 })
-export class LevelOneComponent {
+export class LevelOneComponent {}
 
+const scene = new THREE.Scene();
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.set(0, 0, 5);
+camera.lookAt(0, 0, 0);
+
+const clock = new THREE.Clock();
+function animate() {
+  requestAnimationFrame(animate);
+
+  renderer.render(scene, camera);
 }
+animate();
