c9d9a61d8031655906cf3c45961e2b44e8e85927
AwesomeTevv <tevlen.naidoo@gmail.com>
2023-10-07 18:54:21 +0200

    Added necessary files

---
+++ level-one.js
@@ -0,0 +1,88 @@
+import * as THREE from "three";
+import Stats from "three/examples/jsm/libs/stats.module";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+let scene;
+let camera;
+let renderer;
+let stats;
+let controls;
+
+const clock = new THREE.Clock();
+
+init();
+animate();
+
+window.addEventListener("resize", onWindowResize);
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function worldLight() {
+  const ambientLight = new THREE.AmbientLight(0x404040); // soft white light
+  scene.add(ambientLight);
+
+  const hemisphereLight = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
+  scene.add(hemisphereLight);
+}
+
+function worldPlane() {
+  const geometry = new THREE.PlaneGeometry(100, 100);
+  const material = new THREE.MeshPhongMaterial({
+    color: 0xffff00,
+    side: THREE.DoubleSide,
+  });
+  const plane = new THREE.Mesh(geometry, material);
+  plane.rotateX(-Math.PI / 2);
+  scene.add(plane);
+}
+
+function helpers() {
+  const axesHelper = new THREE.AxesHelper(100);
+  scene.add(axesHelper);
+}
+
+function init() {
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0x88ccee);
+  //   scene.fog = new THREE.Fog(0x88ccee, 0, 50);
+
+  camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.lookAt(0, 0, 0);
+  camera.position.set(10, 10, 10);
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMap.enabled = true;
+  document.body.appendChild(renderer.domElement);
+
+  stats = new Stats();
+  stats.domElement.style.position = "absolute";
+  stats.domElement.style.top = "0px";
+  document.body.appendChild(stats.domElement);
+
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.05;
+
+  worldLight();
+  worldPlane();
+
+  helpers(); // ! Temporary -- Remove at the end
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  stats.update();
+  controls.update();
+
+  renderer.render(scene, camera);
+}
