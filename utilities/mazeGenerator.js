534494c030a2e7024bf415b46c2455d6acd3b399
AwesomeTevv <tevlen.naidoo@gmail.com>
2023-10-16 14:05:06 +0200

    Added textures -- scuffed right now

--- utilities/mazeGenerator.js
+++ utilities/mazeGenerator.js
@@ -4,13 +4,15 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 let scene;
 let maze;
 
+const loader = new THREE.TextureLoader();
+
 function perimeter(maze, scene) {
   const height = 15;
   const length = 100;
   const thickness = 10;
 
   //   const geometry = new THREE.BoxGeometry(length, height, thickness);
-  const material = new THREE.MeshBasicMaterial({ color: 0x0000bb });
+  const material = new THREE.MeshPhongMaterial({ color: 0x0000bb });
 
   for (let i = -5; i < 5; i++) {
     maze.add(
@@ -58,11 +60,42 @@ function perimeter(maze, scene) {
 export function Maze(maze, scene) {
   perimeter(maze, scene);
 
+  // Loading in textures
+  const map = loader.load(
+    "/assets/bricks/BricksReclaimedWhitewashedOffset001_COL_2K_METALNESS.png"
+  );
+  const bmap = loader.load(
+    "/assets/bricks/BricksReclaimedWhitewashedOffset001_BUMP_2K_METALNESS.png"
+  );
+  const dmap = loader.load(
+    "/assets/bricks/BricksReclaimedWhitewashedOffset001_DISP_2K_METALNESS.png"
+  );
+
+  const scale = 10;
+  map.wrapS = map.wrapT = THREE.RepeatWrapping;
+  map.repeat.set(scale, scale / 10);
+
+  bmap.wrapS = bmap.wrapT = THREE.RepeatWrapping;
+  bmap.repeat.set(scale, scale / 10);
+
+  dmap.wrapS = dmap.wrapT = THREE.RepeatWrapping;
+  dmap.repeat.set(scale, scale / 10);
+
+  const material = new THREE.MeshPhongMaterial({
+    specular: 0x666666,
+    shininess: 50,
+    bumpMap: bmap,
+    bumpScale: 0.3,
+    displacementMap: dmap,
+    displacementScale: 0,
+    map: map,
+  });
+
   const length = 105;
   const height = 10;
   const thickness = 1;
 
-  const material = new THREE.MeshBasicMaterial({ color: 0xff00ff });
+  //   const material = new THREE.MeshPhongMaterial({ color: 0xff00ff });
 
   //   const help = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 
