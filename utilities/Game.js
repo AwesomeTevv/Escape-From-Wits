b74adc2d0af7ba6fb32366602fbb7c4518d04361
AwesomeTevv <tevlen.naidoo@gmail.com>
2023-10-20 21:46:46 +0200

    Added hemisphere light

--- utilities/Game.js
+++ utilities/Game.js
@@ -247,6 +247,9 @@ class Game {
     dirLight2.castShadow = true;
     this.scene.add(dirLight2);
 
+    const hemisphereLight = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
+    this.scene.add(hemisphereLight);
+
     const ambientLight = new THREE.AmbientLight(0x555555);
     this.scene.add(ambientLight);
   }
