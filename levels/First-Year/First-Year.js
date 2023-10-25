0364b077dde58647acb854513c93891c4692f047
AwesomeTevv <tevlen.naidoo@gmail.com>
2023-10-25 23:01:46 +0200

    Added Post-Processing to First Year

--- levels/First-Year/First-Year.js
+++ levels/First-Year/First-Year.js
@@ -1,5 +1,14 @@
-import { Game } from "../../utilities/Game";
 import * as THREE from "three";
+
+// Custom Game Class
+import { Game } from "../../utilities/Game";
+
+// Post-Processing Effects
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass";
+import { OutputPass } from "three/examples/jsm/postprocessing/OutputPass";
+import { FilmPass } from "three/examples/jsm/postprocessing/FilmPass";
+import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass";
+import { VignetteShader } from "three/examples/jsm/shaders/VignetteShader";
 
 let _App = null;
 window.addEventListener("DOMContentLoaded", () => {
@@ -14,16 +23,28 @@ class FirstLevel extends Game {
       "Sand/GroundSand005"
       // "desertWall/StuccoRoughCast001_"
     );
-    // const geometry = new THREE.ConeGeometry( 10, 30, 4, 1 );
-    // const material = new THREE.MeshPhongMaterial( { color: 0x000000, flatShading: true } );
-    // for ( let i = 0; i < 500; i ++ ) {
-    //   const mesh = new THREE.Mesh( geometry, material );
-    //   mesh.position.x = Math.random() * 1600 - 800;
-    //   mesh.position.y = 0;
-    //   mesh.position.z = Math.random() * 1600 - 800;
-    //   mesh.updateMatrix();
-    //   mesh.matrixAutoUpdate = false;
-    //   this.scene.add(mesh);
-    // }
+
+    // Add Post-Processing Effects
+    this._PostProcessing();
+  }
+  _PostProcessing() {
+    this.composer.addPass(new RenderPass(this.scene, this.camera));
+
+    const effectFilm = new FilmPass(4);
+    this.composer.addPass(effectFilm);
+
+    const vignette = new ShaderPass(VignetteShader);
+    // vignette.uniforms["resolution"].value = new THREE.Vector2(
+    //   window.innerWidth,
+    //   window.innerHeight
+    // );
+    // vignette.uniforms["horizontal"].value = true; // default is false
+    // vignette.uniforms["radius"].value = 0.8; // default is 0.75
+    // vignette.uniforms["softness"].value = 0.3; // default is 0.3
+    // vignette.uniforms["gain"].value = 0.3; // default is 0.9
+    this.composer.addPass(vignette);
+
+    const outputPass = new OutputPass();
+    this.composer.addPass(outputPass);
   }
 }
