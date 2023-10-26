4fc269c2329c47e8d0c51fd96ef5641cf13057d3
AwesomeTevv <tevlen.naidoo@gmail.com>
2023-10-26 20:59:08 +0200

    Got rid of the hurting effect

--- utilities/Game.js
+++ utilities/Game.js
@@ -5,9 +5,6 @@ import Stats from "three/examples/jsm/libs/stats.module";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
 
-import { BloomPass } from "three/examples/jsm/postprocessing/BloomPass";
-import { AfterimagePass } from "three/examples/jsm/postprocessing/AfterimagePass";
-
 // Cannon-ES Imports
 import * as CANNON from "cannon-es";
 import { PointerLockControlsCannon } from "./PointerLockControlsCannon";
@@ -69,8 +66,6 @@ class Game {
     this.bulletTexture = bulletTexture;
 
     this.exitTexture = exitTexture;
-
-    this.bloom = null;
 
     this.maze = null; // The generated maze of the game
 
@@ -219,7 +214,6 @@ class Game {
     this.rendererMap.setSize(200, 200);
 
     this.composer = new EffectComposer(this.renderer);
-    this.bloom = new AfterimagePass();
 
     //Shader uniform composer
     this.shaderTime = 0.0;
@@ -888,7 +882,6 @@ class Game {
   }
 
   hurt() {
-    this.composer.addPass(this.bloom);
     this.currentHealth -= 1;
     console.log(this.currentHealth);
     if (this.currentHealth < 0 && this.playerLives != 0) {
@@ -967,7 +960,6 @@ class Game {
           if (this.player.position.distanceTo(this.enemyBody[i].position) < 3) {
             this.hurt();
           } else {
-            this.composer.removePass(this.bloom);
           }
         }
       }
